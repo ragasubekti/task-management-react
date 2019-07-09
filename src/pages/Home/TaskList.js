50cf39311a1c4a8e61cd8a3bc90df56880420fcf
Raga Subekti <rfilmriot@gmail.com>
2019-07-09 08:07:31 +0700

    Add Button Styles

--- src/pages/Home/TaskList.js
+++ src/pages/Home/TaskList.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Table } from "antd";
+import styled from "@emotion/styled";
 
 const { Column } = Table;
+
+const DetailButton = styled.button`
+  border-radius: 2.5px;
+  margin: 2px;
+  border: none;
+  color: #fff;
+  background: ${props =>
+    props.primary
+      ? `linear-gradient(to bottom right, #9CECFB, #0052D4)`
+      : props.danger
+      ? `linear-gradient(to bottom right, #da4453, #89216b);`
+      : `linear-gradient(to bottom right, #f46b45, #eea849)`};
+`;
 
 class TaskList extends React.Component {
   render() {
@@ -23,13 +37,13 @@ class TaskList extends React.Component {
             key="_id"
             render={(t, r, i) => (
               <div className="text-right">
-                <button className="btn btn-primary">Detail</button>
-                <button>
+                <DetailButton primary>Detail</DetailButton>
+                <DetailButton>
                   <i className="fa fa-pencil" />
-                </button>
-                <button>
+                </DetailButton>
+                <DetailButton danger>
                   <i className="fa fa-trash" />
-                </button>
+                </DetailButton>
               </div>
             )}
           />
